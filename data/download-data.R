e1707485e6d66721dc75a7d1a7f30e91efeb65a1
nrennie <nrennie35@gmail.com>
2023-10-13 15:14:12 +0100

    add bees to data download

--- data/download-data.R
+++ data/download-data.R
@@ -3,3 +3,11 @@ download.file(
   "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-03-21/languages.csv",
   destfile = "data/languages.csv")
 
+# Bees
+download.file(
+  "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-01-11/colony.csv",
+  destfile = "data/colony.csv")
+download.file(
+  "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-01-11/stressor.csv",
+  destfile = "data/stressor.csv")
+
