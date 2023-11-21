8bc5a09b15cf1f9e7b3708b335f4c15be0333929
nrennie <nrennie35@gmail.com>
2023-11-21 14:18:40 +0000

    add old code examples

--- data/download-data.R
+++ data/download-data.R
@@ -1,9 +1,9 @@
-# Languages
+# Programming languages
 download.file(
   "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-03-21/languages.csv",
   destfile = "data/languages.csv")
 
-# Bees
+# Bee colony losses
 download.file(
   "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-01-11/colony.csv",
   destfile = "data/colony.csv")
@@ -11,3 +11,4 @@ download.file(
   "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-01-11/stressor.csv",
   destfile = "data/stressor.csv")
 
+# UK Museums
