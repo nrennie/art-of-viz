c4866d7ae8d5af974ffbac36636e4a4e06bf779c
nrennie <nrennie35@gmail.com>
2023-11-21 20:44:54 +0000

    add more data

--- data/README.md
+++ data/README.md
@@ -20,6 +20,21 @@ Source: [https://github.com/rfordatascience/tidytuesday/tree/master/data/2022/20
 
 Source: [https://github.com/rfordatascience/tidytuesday/tree/master/data/2022/2022-01-25](https://github.com/rfordatascience/tidytuesday/tree/master/data/2022/2022-01-25)
 
+## Starbucks
+
+Source: [https://github.com/rfordatascience/tidytuesday/tree/master/data/2021/2021-12-21](https://github.com/rfordatascience/tidytuesday/tree/master/data/2021/2021-12-21)
+
+## UFO sightings
+
+Source: [https://github.com/rfordatascience/tidytuesday/tree/master/data/2023/2023-06-20](https://github.com/rfordatascience/tidytuesday/tree/master/data/2023/2023-06-20)
+
+## Cats
+
+Source: [https://github.com/rfordatascience/tidytuesday/tree/master/data/2023/2023-01-31](https://github.com/rfordatascience/tidytuesday/tree/master/data/2023/2023-01-31)
+
+
+
+
 
 
 
--- data/download-data.R
+++ data/download-data.R
@@ -24,6 +24,34 @@ download.file(
   "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-01-25/ratings.csv",
   destfile = "data/ratings.csv")
 
+# Starbucks
+download.file(
+  "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-12-21/starbucks.csv",
+  destfile = "data/starbucks.csv")
+
+# UFO sightings
+download.file(
+  "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-06-20/ufo_sightings.csv",
+  destfile = "data/ufo_sightings.csv")
+download.file(
+  "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-06-20/places.csv",
+  destfile = "data/places.csv")
+download.file(
+  "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-06-20/day_parts_map.csv",
+  destfile = "data/day_parts_map.csv")
+
+# Cats
+download.file(
+  "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-01-31/cats_uk.csv",
+  destfile = "data/cats_uk.csv")
+download.file(
+  "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-01-31/cats_uk_reference.csv",
+  destfile = "data/cats_uk_reference.csv")
+
+
+
+
+
 
 
 
