da52284efd8126ad2747646c167a483e6873f9a7
nrennie <nrennie35@gmail.com>
2023-11-23 12:41:37 +0000

    update code

--- R/01-programming-languages.R
+++ R/01-programming-languages.R
@@ -1,98 +1,255 @@
-library(tidyverse)
-library(showtext)
+# Load packages -----------------------------------------------------------
+
+library(dplyr)
+library(ggplot2)
 library(ggtext)
-library(glue)
+library(readr)
+library(showtext)
+library(sysfonts)
+library(tidyr)
 
-# load fonts
-font_add_google("VT323", "vt")
-font_add_google("Share Tech Mono", "share")
-showtext_auto()
 
-# load data
-languages <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-03-21/languages.csv')
+# Load data ---------------------------------------------------------------
 
-# data wrangling
-plot_data <- languages |> 
-  filter(type == "pl") |> 
-  filter(line_comment_token %in% c("//", "#", ";")) |> 
-  select(title, appeared, line_comment_token, last_activity, language_rank) |> 
-  drop_na() |> 
-  mutate(label = paste("Comment token:", line_comment_token)) |> 
-  group_by(label) |> 
-  slice_head(n = 10) |> 
-  mutate(n = factor(row_number(), levels = 1:10)) |> 
-  ungroup() |> 
-  select(label, n, title, appeared, last_activity, language_rank) |> 
-  pivot_longer(cols = c(appeared, last_activity),
-               names_to = "type",
-               values_to = "year") |>
-  mutate(title = paste0("<p>", title, "</p><p style='font-size:19pt;'>(Rank: ", language_rank, ")</p>"))
+languages <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-03-21/languages.csv")
 
-# text
-social <- "<span style='font-family:\"Font Awesome 6 Brands\";color:#66FF00;'>&#xf099;</span><span style='color:grey5;'>.</span><span style='font-family:share;color:#66FF00;'>@nrennie35</span><span style='color:grey5;'>..</span><span style='font-family:\"Font Awesome 6 Brands\";color:#66FF00;'>&#xf4f6;</span><span style='color:grey5;'>.</span><span style='font-family:share;color:#66FF00;'>fosstodon.org/@nrennie</span><span style='color:grey5;'>..</span><span style='font-family:\"Font Awesome 6 Brands\";color:#66FF00;'>&#xf09b;</span><span style='color:grey5;'>.</span><span style='font-family:share;color:#66FF00;'>nrennie</span><span style='color:grey5;'>..</span>"
+
+# Data wrangling ----------------------------------------------------------
+
+plot_data <- languages |>
+  filter(type == "pl") |>
+  filter(line_comment_token %in% c("//", "#", ";")) |>
+  select(title, appeared, line_comment_token, last_activity, language_rank) |>
+  drop_na() |>
+  mutate(label = paste("Comment token:", line_comment_token)) |>
+  group_by(label) |>
+  slice_head(n = 10) |>
+  mutate(n = factor(row_number(), levels = 1:10)) |>
+  ungroup() |>
+  select(label, n, title, appeared, last_activity, language_rank) |>
+  pivot_longer(
+    cols = c(appeared, last_activity),
+    names_to = "type",
+    values_to = "year"
+  )
+
+# Initial plot ------------------------------------------------------------
+
+basic_plot <- ggplot(data = plot_data) +
+  geom_line(mapping = aes(x = year, y = n, group = n)) +
+  geom_point(mapping = aes(x = year, y = n)) +
+  facet_wrap(~label)
+basic_plot
+
+
+# Re-order and scales -----------------------------------------------------
+
+basic_plot +
+  scale_y_discrete(limits = rev) +
+  scale_x_continuous(
+    breaks = c(1970, 1990, 2010, 2030),
+    limits = c(1930, 2030),
+    expand = c(0, 0)
+  )
+
+
+# Define colours ----------------------------------------------------------
+
+bg_col <- "grey5"
+main_col <- "#66FF00"
+
+
+# Add colour styling ------------------------------------------------------
+
+basic_plot <- ggplot(data = plot_data) +
+  geom_line(
+    mapping = aes(x = year, y = n, group = n),
+    colour = main_col
+  ) +
+  geom_point(
+    mapping = aes(x = year, y = n),
+    colour = main_col,
+    fill = bg_col,
+    pch = 22
+  ) +
+  facet_wrap(~label) +
+  # add scales back in
+  scale_y_discrete(limits = rev) +
+  scale_x_continuous(
+    breaks = c(1970, 1990, 2010, 2030),
+    limits = c(1930, 2030),
+    expand = c(0, 0)
+  )
+basic_plot
+
+
+# Add text as variables ---------------------------------------------------
+
+# Text
+title <- "Programming Languages"
 subtitle <- "Of the 4,303 programming languages listed in the Programming Language DataBase, 205 use //, 101 use #, and 64 use ;
 to define which lines are comments. 3,831 languages do not have a comment token listed."
+caption <- "Data: Programming Language DataBase | Graphic: N. Rennie"
 
-# plot
-ggplot(plot_data) +
-  geom_line(aes(x = year, y = n, group = n),
-            colour = "#66FF00",
-            linewidth = 0.4) +
-  geom_point(aes(x = year, y = n),
-             pch = 22,
-             fill = "grey5",
-             colour = "#66FF00",
-             size = 2) +
-  geom_richtext(aes(x = 1930, y = n, label = title),
-                hjust = 0,
-                family = "share",
-                size = 8,
-                fill = "transparent",
-                label.colour = "transparent",
-                vjust = 0.5,
-                colour = "#66FF00") +
-  facet_wrap(~label, nrow = 1) +
-  scale_y_discrete(limits = rev) +
-  scale_x_continuous(breaks = c(1970, 1990, 2010, 2030),
-                     limits = c(1930, 2030),
-                     expand = c(0, 0)) +
-  labs(x = "",
-       y = "",
-       title = "Programming Languages",
-       subtitle = subtitle,
-       caption = social) +
-  theme_minimal(base_size = 24,
-                base_family = "share") +
-  theme(plot.background = element_rect(fill = "grey5", colour = "grey5"),
-        panel.background = element_rect(fill = "grey10",
-                                        colour = alpha("#66FF00", 0.5),
-                                        linewidth = 0.4),
-        panel.grid.minor = element_blank(),
-        panel.grid.major.x = element_line(colour = alpha("#66FF00", 0.5),
-                                          linewidth = 0.2),
-        panel.grid.major.y = element_blank(),
-        strip.text = element_text(colour = "#66FF00", size = 26),
-        axis.text.y = element_blank(),
-        axis.text.x = element_text(colour = "#66FF00",
-                                   vjust = 2),
-        plot.title = element_text(colour = "#66FF00",
-                                  family = "vt",
-                                  face = "bold",
-                                  size = 60),
-        plot.subtitle = element_textbox_simple(
-          lineheight = 0.4,
-          colour = "#66FF00",
-          hjust = 0,
-          size = 28,
-          margin = margin(b = 5)
-        ),
-        plot.caption = element_textbox_simple(
-          lineheight = 0.4,
-          colour = "#66FF00",
-          hjust = 0,
-          size = 28,
-          margin = margin(t = 5)
-        ),
-        axis.ticks = element_blank(),
-        plot.margin = margin(10, 15, 5, 0))
+text_plot <- basic_plot +
+  labs(
+    title = title,
+    subtitle = subtitle,
+    caption = caption
+  )
+text_plot
 
+
+# Define text and fonts ---------------------------------------------------
+
+# Fonts
+sysfonts::font_add_google("VT323", "vt")
+sysfonts::font_add_google("Share Tech Mono", "share")
+showtext::showtext_auto()
+
+# Font variables
+body_font <- "share"
+title_font <- "vt"
+
+
+# Basic theming -----------------------------------------------------------
+
+theme_plot1 <- text_plot +
+  theme_minimal(
+    base_size = 12,
+    base_family = body_font
+  )
+
+# better styling
+theme_plot1 +
+  theme(
+    plot.background = element_rect(
+      fill = bg_col,
+      colour = bg_col
+    ),
+    panel.background = element_rect(
+      fill = bg_col,
+      colour = alpha(main_col, 0.5),
+      linewidth = 0.4
+    ),
+    panel.grid.minor = element_blank(),
+    panel.grid.major.x = element_line(
+      colour = alpha(main_col, 0.5),
+      linewidth = 0.2
+    ),
+    panel.grid.major.y = element_blank(),
+    strip.text = element_text(
+      colour = main_col
+    ),
+    axis.text.y = element_blank(),
+    axis.text.x = element_text(
+      colour = main_col,
+      vjust = 2
+    ),
+    plot.title = element_text(
+      colour = main_col,
+      family = title_font
+    ),
+    plot.subtitle = element_text(
+      colour = main_col,
+      margin = margin(b = 5)
+    ),
+    plot.caption = element_text(
+      colour = main_col,
+      margin = margin(b = 5),
+      hjust = 0
+    ),
+    axis.ticks = element_blank(),
+    plot.margin = margin(10, 15, 5, 0)
+  )
+
+# make title bigger
+# stop subtitle running off
+# intro to ggtext
+# highlight only some lines
+theme_plot2 <- theme_plot1 +
+  theme(
+    plot.background = element_rect(
+      fill = bg_col,
+      colour = bg_col
+    ),
+    panel.background = element_rect(
+      fill = bg_col,
+      colour = alpha(main_col, 0.5),
+      linewidth = 0.4
+    ),
+    panel.grid.minor = element_blank(),
+    panel.grid.major.x = element_line(
+      colour = alpha(main_col, 0.5),
+      linewidth = 0.2
+    ),
+    panel.grid.major.y = element_blank(),
+    strip.text = element_text(
+      colour = main_col
+    ),
+    axis.text.y = element_blank(),
+    axis.text.x = element_text(
+      colour = main_col,
+      vjust = 2
+    ),
+    plot.title = element_text(
+      colour = main_col,
+      family = title_font,
+      size = 40
+    ),
+    plot.subtitle = element_textbox_simple(
+      colour = main_col,
+      margin = margin(b = 5)
+    ),
+    plot.caption = element_text(
+      colour = main_col,
+      margin = margin(b = 5),
+      hjust = 0
+    ),
+    axis.ticks = element_blank(),
+    plot.margin = margin(10, 15, 5, 0)
+  )
+theme_plot2
+
+# add labels of code language
+theme_plot2 +
+  geom_text(
+    mapping = aes(x = 1930, y = n, label = title),
+    family = body_font,
+    fill = "transparent",
+    label.colour = "transparent",
+    vjust = 0.5,
+    hjust = 0,
+    colour = main_col
+  )
+
+# make a better label
+plot_data <- plot_data |>
+  mutate(rank_label = glue::glue("{title}\nRank: {language_rank}"))
+
+theme_plot2 +
+  geom_text(
+    data = plot_data,
+    mapping = aes(x = 1930, y = n, label = rank_label),
+    family = body_font,
+    vjust = 0.5,
+    hjust = 0,
+    colour = main_col
+  )
+
+# make an HTML label
+plot_data <- plot_data |>
+  mutate(rank_label = paste0("<p>", title, "</p><p style='font-size:8pt;'>(Rank: ", language_rank, ")</p>"))
+
+# plot with geom_richtext
+theme_plot2 +
+  geom_richtext(
+    data = plot_data,
+    mapping = aes(x = 1930, y = n, label = rank_label),
+    family = body_font,
+    fill = "transparent",
+    label.colour = "transparent",
+    vjust = 0.5,
+    hjust = 0,
+    colour = main_col
+  )
