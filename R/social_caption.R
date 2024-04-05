5d1695b8ee64304383eec89c30465e8f6148ea07
nrennie <nrennie35@gmail.com>
2024-04-05 18:57:38 +0100

    add R files

---
+++ R/social_caption.R
@@ -0,0 +1,30 @@
+social_caption <- function(twitter = "@nrennie35",
+                           mastodon = "fosstodon.org/@nrennie",
+                           linkedin = "nicola-rennie",
+                           github = "nrennie",
+                           icon_colour = nr_dark,
+                           font_colour = nr_dark,
+                           bg_colour = nr_light,
+                           font_family = "Commissioner") {
+  icons <- list(
+    twitter = "&#xf099",
+    mastodon = "&#xf4f6",
+    linkedin = "&#xf08c",
+    github = "&#xf09b"
+  )
+  social <- list(
+    twitter = twitter,
+    mastodon = mastodon,
+    linkedin = linkedin,
+    github = github
+  )
+  social <- social[!is.na(social)]
+  caption <- ""
+  for (name in names(social)) {
+    icon <- icons[name]
+    info <- social[name]
+    html <- glue::glue("<span style='font-family:\"Font Awesome 6 Brands\";color:{icon_colour};'>{icon};</span><span style='color:{bg_colour};'>.</span><span style='font-family:{font_family};color:{font_colour};'>{info}</span><span style='color:{bg_colour};'>..</span>")
+    caption <- paste0(caption, html)
+  }
+  return(caption)
+}
