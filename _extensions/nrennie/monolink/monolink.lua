8114d76e63c968656ac471bf4f283ed89195ffcf
Nicola Rennie <42338476+nrennie@users.noreply.github.com>
2025-08-25 21:14:14 +0100

    minor edits after rendering

--- _extensions/nrennie/monolink/monolink.lua
+++ _extensions/nrennie/monolink/monolink.lua
@@ -16,6 +16,13 @@ local function escape_latex(str)
   end))
 end
 
+local function add_breakpoints(str)
+  -- Add discretionary line breaks after / and -
+  str = str:gsub("/", "/\\allowbreak{}")
+  str = str:gsub("%-", "-\\allowbreak{}")
+  return str
+end
+
 function monolink(args, kwargs, meta)
   local url = pandoc.utils.stringify(args[1])
   local label = args[2] and pandoc.utils.stringify(args[2]) or url
@@ -23,6 +30,10 @@ function monolink(args, kwargs, meta)
   if quarto.doc.is_format("pdf") then
     url = escape_latex(url)
     label = escape_latex(label)
+    
+    -- Add line break opportunities to the displayed label only
+    label = add_breakpoints(label)
+    
     return pandoc.RawInline("latex", "\\texttt{\\href{" .. url .. "}{" .. label .. "}}")
   else
     return pandoc.Link(label, url)
